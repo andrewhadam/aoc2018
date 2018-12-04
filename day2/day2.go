645b977c21d5b1012d8765be87714a49c1465f37
Andrew Hadam <ahadam@ath033818.corp.athenahealth.com>
2018-12-04 16:34:47 -0500

    initial work - does not produce correct answers but done for the day

--- day2/day2.go
+++ day2/day2.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,11 +17,36 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	var twoChar = 0
+	var threeChar = 0
 
 	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		var str = scanner.Text()
+		fmt.Println("OG String: ", str)
+		for len(str) > 0 {
+			//for ; spos, char := range str {
+			fmt.Println("Removed chat", string(str[0]))
+			if strings.Count(str, string(str[0])) == 2 {
+				twoChar++
+				fmt.Println("Add two")
+			} else if strings.Count(str, string(str[0])) == 3 {
+				threeChar++
+				fmt.Println("Add three")
+			} else {
+				fmt.Println("Neither")
+			}
+			str = strings.Replace(str, string(str[0]), "", -1)
+			fmt.Println("String post manipulation", str)
+			//			str = strings.Replace(str, string(char), "", -1)
+			fmt.Println("Replacement String: ", str)
+			//fmt.Println(pos, char)
+		}
+		//		var string = "blahblah"
+		//		duplicates = string.chars.group_by { |char| char }.select { |key, value| value.size > 1 }
 
 	}
-
+	fmt.Println(twoChar, threeChar)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
