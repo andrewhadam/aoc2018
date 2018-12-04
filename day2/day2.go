d44b030d07b486f6a4f76f8904a28c95e477df62
Andrew Hadam <ahadam@ath033818.corp.athenahealth.com>
2018-12-04 16:49:58 -0500

    day2 part 1 fixed

--- day2/day2.go
+++ day2/day2.go
@@ -24,27 +24,25 @@ func main() {
 		fmt.Println(scanner.Text())
 		var str = scanner.Text()
 		fmt.Println("OG String: ", str)
+		var twoTrue = false
+		var threeTrue = false
 		for len(str) > 0 {
-			//for ; spos, char := range str {
 			fmt.Println("Removed chat", string(str[0]))
-			if strings.Count(str, string(str[0])) == 2 {
+			if strings.Count(str, string(str[0])) == 2 && twoTrue == false {
 				twoChar++
+				twoTrue = true
 				fmt.Println("Add two")
-			} else if strings.Count(str, string(str[0])) == 3 {
+			} else if strings.Count(str, string(str[0])) == 3 && threeTrue == false {
 				threeChar++
+				threeTrue = true
 				fmt.Println("Add three")
 			} else {
 				fmt.Println("Neither")
 			}
 			str = strings.Replace(str, string(str[0]), "", -1)
 			fmt.Println("String post manipulation", str)
-			//			str = strings.Replace(str, string(char), "", -1)
 			fmt.Println("Replacement String: ", str)
-			//fmt.Println(pos, char)
 		}
-		//		var string = "blahblah"
-		//		duplicates = string.chars.group_by { |char| char }.select { |key, value| value.size > 1 }
-
 	}
 	fmt.Println(twoChar, threeChar)
 	if err := scanner.Err(); err != nil {
